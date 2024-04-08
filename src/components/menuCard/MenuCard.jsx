0d1dafc56ed85815167dccf2d0b7297eb60c5ebe
nursultan1196 <nursultan1196@gmail.com>
2024-04-08 20:19:18 +0600

    TT2-50<Add menu details modal window>

--- src/components/menuCard/MenuCard.jsx
+++ src/components/menuCard/MenuCard.jsx
@@ -1,10 +1,19 @@
 import "./menuCardStyles.css";
 import coffeeImage from "../../assets/coffee1.png"
+import {useDispatch} from "react-redux"
+import {openModal} from "../../redux"
 
 
 function MenuCard(){
-
-
+ const dispatch = useDispatch();
+ 
+ const handleOpenModal = (modalName) => {
+    dispatch(
+      openModal({
+        name: modalName,
+      })
+    );
+  };
 
     return(
         <div className="menu-card">
@@ -13,9 +22,13 @@ function MenuCard(){
                   src={coffeeImage} 
                   alt="coffeeImage" 
                   className="menu-card-image"
+                  onClick={()=>handleOpenModal("menuItemDetails")}
                 />
             </div>
-            <div className="menu-card-text-container">
+            <div 
+              className="menu-card-text-container"
+              onClick={()=>handleOpenModal("menuItemDetails")}
+            >
                 <p className="menu-card-name">Капучино</p>
                 <p className="menu-card-price">140 с</p>
             </div>
--- src/modalWindows/menuItemDetails/MenuItemDetails.jsx
+++ src/modalWindows/menuItemDetails/MenuItemDetails.jsx
@@ -2,38 +2,47 @@ import "./menuItemDetailsStyles.css";
 import { InputAdornment, IconButton } from "@mui/material";
 import CloseIcon from '@mui/icons-material/Close';
 import cheeseCake from "../../assets/cheeseCake.png"
+import {useDispatch} from "react-redux"
+import {closeModal} from "../../redux"
 
 function MenuItemDetails(){
+  const dispatch = useDispatch();
 
-
+  const handleCloseModal = ()=>{
+    dispatch(closeModal())
+  }
     
     return(
         <div className="menu-item-details">
              <header className="menu-item-detais-header">
-                <div className="menu-item-details-container">
-                    <img 
-                      src={cheeseCake} 
-                      alt="menu item image"
-                      className="menu-item-details-image" 
-                    />
-                </div>
-                <div className="details-description-container">
-                  <p className="details-description">Чизкейк - сладкий десерт, состоящий из одного или нескольких слоев.</p>
-                </div>
-                
+                <p className="menu-item-details-name">Чизкейк</p>
                 <InputAdornment 
                   position="end"
                   className="menu-item-details-close-icon"
-                >
-                    <IconButton>
-                        <CloseIcon style={{color:"white", width:"30px", height:"30px"}}/>
-                    </IconButton>
-                </InputAdornment>
+                  onClick={handleCloseModal}
+                 >
+                  <IconButton>
+                    <CloseIcon style={{color:"#2A3440", width:"30px", height:"30px"}}/>
+                  </IconButton>
+                </InputAdornment> 
             </header>
+
+            <div className="menu-item-detais-subContainer">
+              <div className="menu-item-details-container">
+                <img 
+                  src={cheeseCake} 
+                  alt="menu item image"
+                  className="menu-item-details-image" 
+                />
+              </div>
+              <div className="details-description-container">
+                <p className="details-description">Чизкейк - сладкий десерт, состоящий из одного или нескольких слоев.</p>
+              </div>
+            </div>
 
             <div className="menu-item-ingredients-container">
                 <p className="menu-item-ingredients-title">Основные ингредиенты:</p>
-                <ul>
+                <ul className="menu-item-ingredients-title-list">
                     <li className="menu-item-ingredients">печенье (песочное) – 300 гр;</li>
                     <li className="menu-item-ingredients">масло – 100 гр;</li>
                     <li className="menu-item-ingredients">мускатный орех</li>
